041ba80979653de37a51272ae6c7419909f6c3db
Gonzalo Garzon <tato_garzon@hotmail.com>
2022-08-10 18:17:19 -0300

    Punto Seguro

---
+++ AppCoder/views.py
@@ -0,0 +1,107 @@
+from django.http.request import QueryDict
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from AppCoder.models import Curso, Profesor
+from AppCoder.forms import CursoFormulario, ProfesorFormulario
+
+# Create your views here.
+
+def curso(request):
+
+      curso =  Curso(nombre="Desarrollo web", camada="19881")
+      curso.save()
+      documentoDeTexto = f"--->Curso: {curso.nombre}   Camada: {curso.camada}"
+
+
+      return HttpResponse(documentoDeTexto)
+
+
+def inicio(request):
+
+      return render(request, "AppCoder/inicio.html")
+
+
+
+def estudiantes(request):
+
+      return render(request, "AppCoder/estudiantes.html")
+
+
+def entregables(request):
+
+      return render(request, "AppCoder/entregables.html")
+
+
+def cursos(request):
+
+      if request.method == 'POST':
+
+            miFormulario = CursoFormulario(request.POST) #aquí mellega toda la información del html
+
+            print(miFormulario)
+
+            if miFormulario.is_valid:   #Si pasó la validación de Django
+
+                  informacion = miFormulario.cleaned_data
+
+                  curso = Curso (nombre=informacion['curso'], camada=informacion['camada']) 
+
+                  curso.save()
+
+                  return render(request, "AppCoder/inicio.html") #Vuelvo al inicio o a donde quieran
+
+      else: 
+
+            miFormulario= CursoFormulario() #Formulario vacio para construir el html
+
+      return render(request, "AppCoder/cursos.html", {"miFormulario":miFormulario})
+
+
+
+
+def profesores(request):
+
+      if request.method == 'POST':
+
+            miFormulario = ProfesorFormulario(request.POST) #aquí mellega toda la información del html
+
+            print(miFormulario)
+
+            if miFormulario.is_valid:   #Si pasó la validación de Django
+
+                  informacion = miFormulario.cleaned_data
+
+                  profesor = Profesor (nombre=informacion['nombre'], apellido=informacion['apellido'],
+                   email=informacion['email'], profesion=informacion['profesion']) 
+
+                  profesor.save()
+
+                  return render(request, "AppCoder/inicio.html") #Vuelvo al inicio o a donde quieran
+
+      else: 
+
+            miFormulario= ProfesorFormulario() #Formulario vacio para construir el html
+
+      return render(request, "AppCoder/profesores.html", {"miFormulario":miFormulario})
+
+
+
+
+
+
+def buscar(request):
+
+      if  request.GET["camada"]:
+
+	      #respuesta = f"Estoy buscando la camada nro: {request.GET['camada'] }" 
+            camada = request.GET['camada'] 
+            cursos = Curso.objects.filter(camada__icontains=camada)
+
+            return render(request, "AppCoder/inicio.html", {"cursos":cursos, "camada":camada})
+
+      else: 
+
+	      respuesta = "No enviaste datos"
+
+      #No olvidar from django.http import HttpResponse
+      return HttpResponse(respuesta)
